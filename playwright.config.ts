451aa8ad7502de7c781a25934fb9c13447949113
wagner ruiz <wruizlg@gmail.com>
2026-07-16 16:34:51 +0200

    Configure test execution settings

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,10 +26,12 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: process.env.BASE_URL || 'https://example.com',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
