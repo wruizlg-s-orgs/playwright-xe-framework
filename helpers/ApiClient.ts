23f49d00800d983c4f198ee98fb2b099d1d59536
wagner ruiz <wruizlg@gmail.com>
2026-07-17 17:42:19 +0200

    Adjust apiClient

--- helpers/ApiClient.ts
+++ helpers/ApiClient.ts
@@ -1,13 +1,58 @@
-class ApiClient {
+import {
+    APIRequestContext,
+    APIResponse,
+    request
+} from '@playwright/test';
 
-    initialize()
+export class ApiClient {
 
-    get()
+    private api!: APIRequestContext;
 
-    post()
+    async initialize() {
 
-    put()
+        this.api = await request.newContext({
 
-    delete()
+            baseURL: 'https://reqres.in',
+
+            extraHTTPHeaders: {
+                'Content-Type': 'application/json',
+                'x-api-key': 'reqres-free-v1'
+            }
+
+        });
+
+    }
+
+    async get(endpoint: string): Promise<APIResponse> {
+
+        return await this.api.get(endpoint);
+
+    }
+
+    async post(endpoint: string, body: object): Promise<APIResponse> {
+
+        return await this.api.post(endpoint, {
+
+            data: body
+
+        });
+
+    }
+
+    async put(endpoint: string, body: object): Promise<APIResponse> {
+
+        return await this.api.put(endpoint, {
+
+            data: body
+
+        });
+
+    }
+
+    async delete(endpoint: string): Promise<APIResponse> {
+
+        return await this.api.delete(endpoint);
+
+    }
 
 }
